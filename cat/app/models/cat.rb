febc9e997d68e4be4c7f98db320dd125ffbc6661
junghyun2006 <jung.hyun.park516@gmail.com>
2020-12-04 14:59:12 -0500

    Finish age method

--- cat/app/models/cat.rb
+++ cat/app/models/cat.rb
@@ -2,19 +2,16 @@ require 'action_view'
 require 'date'
 
 class Cat < ApplicationRecord 
+    include ActionView::Helpers::DateHelper
     validates :birth_date, :color, :name, :sex, :description, presence: true
     COLORS = ['Black', 'White', 'Brown', 'Orange']
     validates :color, inclusion: { in: COLORS}
     validates :sex, length: { maximum:1, too_long: "1 characters is the maximum allowed"}
 
     def age
-        d = Date.parse(self.birth_date)
-        from_time = Time.now - d.year - d.mon - d.mday
-        time_ago_in_words(from_time)          
+        # d = Date.parse(self.birth_date)
+        # from_time = Time.now - d.year - d.mon - d.mday
+        time_ago_in_words(self.birth_date)          
     end
-
-
-
-
-
+    
 end
